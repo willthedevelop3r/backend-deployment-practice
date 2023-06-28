59e34f3188ca58aaa73bc5115ccb017ff4f1202c
William Nguyen <williamnguyen773@gmail.com>
2023-06-28 10:54:55 -0500

    comment out console log

--- api/db/connection.js
+++ api/db/connection.js
@@ -2,6 +2,6 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../../knexfile')[env];
 const knex = require('knex')(config);
 
-console.log('/////', env);
+// console.log('/////', env);
 
 module.exports = knex;
