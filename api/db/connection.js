18a56ca2eb351f7f84a5408c189cbfcb61213573
William Nguyen <williamnguyen773@gmail.com>
2023-06-28 10:54:09 -0500

    initial commit

---
+++ api/db/connection.js
@@ -0,0 +1,7 @@
+const env = process.env.NODE_ENV || 'development';
+const config = require('../../knexfile')[env];
+const knex = require('knex')(config);
+
+console.log('/////', env);
+
+module.exports = knex;
---
+++ knexfile.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const {
+  NODE_ENV = 'production',
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
+} = process.env;
+
+// console.log(NODE_ENV, '/////////////');
+
+const URL =
+  NODE_ENV === 'production'
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
+
+// console.log('///////', URL);
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: URL,
+    migrations: {
+      directory: __dirname + '/api/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/api/db/seeds',
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: URL,
+    migrations: {
+      directory: __dirname + '/api/db/migrations',
+    },
+    seeds: {
+      directory: __dirname + '/api/db/seeds',
+    },
+  },
+};
